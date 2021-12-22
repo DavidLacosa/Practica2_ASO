fd0f49647bcf6293f9388126cf5dd8b6d41386b8
David Larrosa Camps <david.larrosa@students.salle.url.edu>
2021-12-22 12:36:02 +0100

    S'afegeix text sota del boto entrar

---
+++ src/js/code.js
@@ -0,0 +1,10 @@
+function enviar(){
+    var nom = document.getElementById("usuari").Value;
+    
+    var text = document.createElement('p');
+    text.innerHTML = "TEXT";
+    text.style = "font-size: 10px; font-weight: 100;"
+    var centrada = document.getElementById("centrada").appendChild(text);
+
+    
+}
