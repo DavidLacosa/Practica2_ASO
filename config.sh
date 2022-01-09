abc2626695365fcea0247e5e6a5c90046f0292b0
David <david.larrosa@students.salle.url.edu>
2022-01-09 23:34:16 +0100

    Network, config i canvis a main

---
+++ config.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo chmod +x ./main.sh
+
+sudo chmod +x ./cron_tasks/cron_tasks.sh
+
+sudo chmod +x ./log_manager/log_manager.sh
+
+sudo chmod +x ./monotoritzacio/monotoritzacio.sh
+
+sudo chmod +x ./network/network.sh
+
+sudo chmod +x ./process_manager/go_to_sleep.sh
+sudo chmod +x ./process_manager/kill_process.sh
+sudo chmod +x ./process_manager/pid_manager.sh
+sudo chmod +x ./process_manager/process_manager.sh
+sudo chmod +x ./process_manager/sleep_process.sh
+
+sudo chmod +x ./ServerButtons/reboot.sh
+sudo chmod +x ./ServerButtons/shutDown.sh
+
+sudo chmod +x ./user_manager/register_user.sh
+sudo chmod +x ./user_manager/remove_user.sh
+sudo chmod +x ./user_manager/user_manager.sh
